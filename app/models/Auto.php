fd87e2369e2b4da5c5b05b55ce23d7d8fab6e57a
boderoode <bo_deroode@outlook.com>
2023-01-12 20:03:25 +0100

    Query aangepast

--- app/models/Auto.php
+++ app/models/Auto.php
@@ -9,21 +9,12 @@ Class Auto {
     }
     public function GetAuto()
     {
-        $this->db->query('SELECT auto.Type, auto.Kenteken, kilometerstand.Id
-                          From auto 
-                          INNER JOIN kilometerstand on auto.Id = kilometerstand.AutoId
+        $this->db->query('SELECT auto.Type, auto.Kenteken, 
+        FROM auto
                           ');
         $result = $this->db->resultSet();
         return $result;
     }
 
-    public function addKmStand()
-    {
-        $sql = "INSERT INTO kilometerstand(Id, KmStand) 
-                VALUES                    (:Id, :KmStand)";
-        $this->db->query($sql);
-        $this->db->bind(':Id', $_POST('Id'), PDO::PARAM_INT);
-        $this->db->bind(':KmStand', $_POST('KmStand'), PDO::PARAM_INT);
-        return $this->db->execute();
-    }
+    
 }
--- app/views/garage/AddKmStand.php
+++
@@ -1,6 +0,0 @@
-<?php require(APPROOT . '/views/includes/header.php'); ?>
-
-<h3><?= $data['title']; ?></h3>
-
-
-<?php require(APPROOT . '/views/includes/footer.php'); ?>
